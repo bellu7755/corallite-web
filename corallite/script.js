bb05edf8e0508e3c0e012560a026eb8572461a3a
Bella <bella@mac.esuhsd.org>
2026-05-18 10:47:54 -0700

    updated homepage layout

--- corallite/script.js
+++ corallite/script.js
@@ -1,3 +1,14 @@
-function clicked() {
-  alert("Website is working!");
+function goPaypal() {
+  window.open("https://www.paypal.com/paypalme/YourLink", "_blank");
 }
+const elements = document.querySelectorAll('.fadeUp');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+elements.forEach(el => observer.observe(el));
