66536ac2e6367d420f9ace7bfb5ca7bfb9b6ac7c
charlotteetay <mushroomcet@gmail.com>
2026-05-19 12:08:20 -0700

    sidebar

--- corallite/script.js
+++ corallite/script.js
@@ -12,3 +12,11 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 elements.forEach(el => observer.observe(el));
+
+function openMenu() {
+  document.getElementById("sideMenu").style.width = "250px";
+}
+
+function closeMenu() {
+  document.getElementById("sideMenu").style.width = "0";
+}
